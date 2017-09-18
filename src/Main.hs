5651f7656162bccd16cb64461d5db28bccf0e7cd
Alexandre Peyroux <alex@xn--wxa.email>
2017-09-18 10:46:17 +0200

    isMuted state

--- src/Main.hs
+++ src/Main.hs
@@ -14,9 +14,15 @@ vol :: IO Integer
 vol = withMixer "default" $ \mixer ->
   do Just control <- getControlByName mixer "Master"
      let Just playbackVolume = playback $ volume control
-     (min, max) <- getRange playbackVolume
+     let Just playbackSwitch = playback $ switch control
+     Just sw <- getChannel FrontLeft playbackSwitch
      Just v <- getChannel FrontLeft $ value $ playbackVolume
-     return $ div (div (v * 100) max) 10
+     (min, max) <- getRange playbackVolume
+     return $ div (div ((isMuted sw v) * 100) max) 10
+     where
+       isMuted sw v | sw == False = 0
+                    | sw == True = v
+                    | otherwise = v
 
 main :: IO ()
 main = do
