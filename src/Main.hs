d2a64695bc0f57982facf6567810c129941a9b38
Alexandre Peyroux <alex@xn--wxa.email>
2018-08-01 10:02:34 +0200

    syntax

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Data.Monoid
 import System.Environment
 import Sound.ALSA.Mixer
+import qualified Data.Text as T
 
-volbar :: Int -> String
-volbar x | x == 0 = "<fc=#6272a4>" ++ take 10 (replicate x '#') ++ "</fc>" ++ take (10-x) $ repeat ':'
-         | x <= 3 = "<fc=#50fa7b>" ++ take 10 (replicate x '#') ++ "</fc>" ++ take (10-x) $ repeat ':'
-         | x >= 3 && x <= 7 = "<fc=#ffb86c>" ++ take 10 $ replicate x '#' ++ "</fc>" ++ take (10-x) $ repeat ':'
-         | x >= 7 = "<fc=#ff5555>" ++ take 10 $ replicate x '#' ++ "</fc>" ++ take (10-x) $ repeat ':'
+volbar :: Int -> Text
+volbar x | x == 0 = "<fc=#6272a4>" <> take 10 $ replicate x '#' <> "</fc>" <> take (10-x) $ repeat ':'
+         | x <= 3 = "<fc=#50fa7b>" <> take 10 $ replicate x '#' <> "</fc>" <> take (10-x) $ repeat ':'
+         | x >= 3 && x <= 7 = "<fc=#ffb86c>" <> take 10 $ replicate x '#' <> "</fc>" <> take (10-x) $ repeat ':'
+         | x >= 7 = "<fc=#ff5555>" <> take 10 $ replicate x '#' <> "</fc>" <> take (10-x) $ repeat ':'
 
 vol :: IO Integer
 vol = withMixer "default" $ \mixer ->
   do Just control <- getControlByName mixer "Master"
      let Just playbackVolume = playback $ volume control
-     let Just playbackSwitch = playback $ switch control
+     let Just playbackSwitch = playback $ switch controlh
      Just sw <- getChannel FrontLeft playbackSwitch
      Just v <- getChannel FrontLeft $ value playbackVolume
      (min, max) <- getRange playbackVolume
